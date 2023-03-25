a0de5a8fa2ceef97a0543aad60a457406a0047c3
HYEONWOO JIN <49802982+maizer2@users.noreply.github.com>
2023-03-25 17:06:38 +0900

    Create utils.py

---
+++ list_control/utils.py
@@ -0,0 +1,19 @@
+import os
+from typing import List
+
+def difference_set_to_list(list1: List, list2: List) -> List:
+    set1 = set(list1)
+    set2 = set(list2)
+    diff = set1 - set2
+    return list(diff)
+ 
+def difference_set_to_set(list1: List, list2: List) -> List:
+    set1 = set(list1)
+    set2 = set(list2)
+    diff = set1 - set2
+    return diff
+
+def list_str_remove(list1: List, prefix: str) -> List:
+    for i in range(len(my_list)):
+        my_list[i] = my_list[i].replace(prefix, '', 1)
+    return my_list
